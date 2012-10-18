ebdf251c58f7d9a9123dc9d61cd53562004012d8
Benjamin Pick <benjamin4ruby@github.com>
2012-10-18 16:59:24 +0200

    app

--- start-crawler.sh
+++ start-crawler.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
 . build.properties
+export LOCK_FILE="/var/run/regain-crawler-live.run"
+export FOLDER="/var/www/regain/regain-svn/build/runtime/crawler-live"
 
 if [ -e $crawler_lockupdate_file_live ] ; then
 	echo "Cannot start crawler: there is a deployment pending."
